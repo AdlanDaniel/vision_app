d55f2a431c42509a2c86d0bd9ce548bc7237a7a4
Daniel Adlan <danieladlan4@gmail.com>
2025-09-18 15:17:58 -0300

    feat/config_firebase_in_projetc

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:vision_app/application.dart';
 import 'package:vision_app/core/environment/environment.dart';
 import 'package:vision_app/core/flavor/flavor_config.dart';
 import 'package:vision_app/core/injection/injection.dart';
+import 'package:vision_app/firebase_options.dart';
+
+
 
 void main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
+
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   await dotenv.load(fileName: ".env");
   final environment = DefaultEnvironment(dotenv);
   InjectionManager.init(  InjectionGetIt());
